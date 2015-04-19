5b56cbee7fc850b7ae057d867d09687c95850078
stevendnichols <nichols.steven.d@gmail.com>
2015-04-19 12:08:56 -0500

    multiply function and preprocessor checks

--- logicalmath.c
+++ logicalmath.c
@@ -1,8 +1,10 @@
 
 const unsigned int UINT_ALL_SET = ~0;
+#ifndef UINT_MAX
 const unsigned int UINT_MAX = ~0;
 const int INT_MAX = (int)(((unsigned int)~0) >> 1);
 const int INT_MIN = ~(((unsigned int)~0) >> 1);
+#endif
 const unsigned int INT_HIGH_BIT = ~(((unsigned int)~0) >> 1);
 
 int add(int m, int n)
@@ -29,7 +31,7 @@ int increment(int n)
    return add(n, 1);
 }
 
-int decrement(n)
+int decrement(int n)
 {
    return add(n, negative(1));
 }
@@ -65,4 +67,22 @@ int lesserThan(int m, int n)
 {
    return greaterThan(n, m);
 }
+
+int multiply(int m, int n)
+{
+  int sum=0, shiftbit=1;
+
+  while(shiftbit) {
+     if(shiftbit & n) {
+        sum = add(sum, m);
+     }
+     m <<= 1;
+     shiftbit <<= 1;
+  }
+  return sum;
+}
+
+
+
+
 
