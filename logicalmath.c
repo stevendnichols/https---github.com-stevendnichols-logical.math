d09c711f9f3f029497ee6f60162d784efba5e311
stevendnichols <nichols.steven.d@gmail.com>
2015-04-19 04:20:55 -0500

    added constants

--- logicalmath.c
+++ logicalmath.c
@@ -1,4 +1,10 @@
 
+const unsigned int UINT_ALL_SET = ~0;
+const unsigned int UINT_MAX = ~0;
+const int INT_MAX = (int)(((unsigned int)~0) >> 1);
+const int INT_MIN = ~(((unsigned int)~0) >> 1);
+const unsigned int INT_HIGH_BIT = ~(((unsigned int)~0) >> 1);
+
 int add(int m, int n)
 {
    int carry = (m & n) << 1;
@@ -8,7 +14,7 @@ int add(int m, int n)
    return m^n;
 }
 
-int equals(int m, int n)
+int equal(int m, int n)
 {
    return !(m^n); 
 }
